ad4007d2080e4900336a418716187c34bc2a13f7
Nicola Gigante <nicolagigante@outlook.com>
2019-08-18 15:47:46 +0200

    Bugfixes, modified README.md

--- config/website.js
+++ config/website.js
@@ -4,14 +4,14 @@ module.exports = {
   titleTemplate: "%s", // Template for Page Titles
   description:
     "UCL Fleming Society's Official Website",
-  siteUrl: "https://gatsby-starter-material-business-markdown.netlify.com", // Domain of your site. No trailing slash!
-  image: "/images/anoun-share-image.png", // Used for SEO Share Image
+  siteUrl: "https://fleming.surge.sh", // Domain of your site. No trailing slash!
+  image: "/images/fleming-share-image.png", // Used for SEO Share Image
 
   // Manifest
   icon: "src/favicon.png", // Used for manifest favicon generation
   short_name: "Fleming", // shortname for manifest. MUST be shorter than 12 characters
   owner: "Fleming", // Author
-  theme_color: "#663399",
+  theme_color: "#ABCC00",
   background_color: "#ffffff",
 
   // Social and Analytics
